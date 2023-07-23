6df955c5d33ff6576d9e7705f0cc6e0b3b7fdc68
sanny085 <sannykumar085@gmail.com>
2023-07-23 20:04:29 +0530

    Implemented dynamic route and blog details page

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = {
-    path('', views.start, name="start-page"),
-    path('posts', views.posts, name="posts-page"),
-    path('posts/<slug:slug>', views.posts_detail, name="posts-detail-page"),
-}
+urlpatterns = [
+    path('', views.start, name='starting-page'),
+    path('posts/', views.posts, name="posts-page"),
+    path('posts/<slug:slug>/', views.posts_detail, name="posts-detail-page"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,97 @@
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-all_news = [
+all_posts = [
     {
-        'id': 1,
-        'title': 'Plants can both hear and produce sounds',
-        'img_url': 'https://images.indianexpress.com/2023/04/flowers-1200.jpg?w=640',
-        'description': 'It’s coming: that great green scream that will shatter our eardrums and change our way of thinking forever! Way back in the 1970s, Peter Tompkins and Christopher Bird published their sensational The Secret Life of Plants (1973), which, among other things, proposed that plants could communicate through sound, only we were unable to hear what they were saying (or screaming). Many of the extraordinary claims made in that book were, however, unsubstantiated, and, to the relief of most of us with sensitive consciences and sentiments, the book withered and died. But it seems that it should still give us pause to think.'
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Raushan",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
     },
     {
-        'id': 1,
-        'title': 'Plants can both hear and produce sounds',
-        'img_url': 'https://images.indianexpress.com/2023/04/flowers-1200.jpg?w=640',
-        'description': 'It’s coming: that great green scream that will shatter our eardrums and change our way of thinking forever! Way back in the 1970s, Peter Tompkins and Christopher Bird published their sensational The Secret Life of Plants (1973), which, among other things, proposed that plants could communicate through sound, only we were unable to hear what they were saying (or screaming). Many of the extraordinary claims made in that book were, however, unsubstantiated, and, to the relief of most of us with sensitive consciences and sentiments, the book withered and died. But it seems that it should still give us pause to think.'
+        "slug": "programming-is-fun",
+        "image": "woods.jpg",
+        "author": "Sanny",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
     },
     {
-        'id': 1,
-        'title': 'Plants can both hear and produce sounds',
-        'img_url': 'https://images.indianexpress.com/2023/04/flowers-1200.jpg?w=640',
-        'description': 'It’s coming: that great green scream that will shatter our eardrums and change our way of thinking forever! Way back in the 1970s, Peter Tompkins and Christopher Bird published their sensational The Secret Life of Plants (1973), which, among other things, proposed that plants could communicate through sound, only we were unable to hear what they were saying (or screaming). Many of the extraordinary claims made in that book were, however, unsubstantiated, and, to the relief of most of us with sensitive consciences and sentiments, the book withered and died. But it seems that it should still give us pause to think.'
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Mohan",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
     }
 ]
 
+def get_date(post):
+    return post['date']
 
 def start(request):
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_post = sorted_posts[-3:]
     return render(request, 'blog/index.html', {
-        'all_news': all_news
+        'latest_post': latest_post
     })
 
 
 def posts(request):
-    return HttpResponse('Blog Post')
+    return render(request, 'blog/all-posts.html', {
+        'all_posts': all_posts
+    })
 
 
-def posts_detail(request):
-    return HttpResponse('Blog Post details')
+def posts_detail(request, slug):
+     
+    current_post = {}
+    for post in all_posts:
+        if post['slug'] == slug:
+            current_post = post
+    return render(request, 'blog/post-detail.html', {
+        'current_post': current_post
+    })
